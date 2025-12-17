3f3aec66c1691e1f17b2a2873ed298da8d575eb2
syedsabaali-1221 <syedsabaali1221@gmail.com>
2025-12-17 19:29:52 +0530

    Added App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+let display=document.getElementById("display");
+
+function press(value) {
+    display.value+=value;
+}
+
+function calculate() {
+    try{
+        display.value=eval(display.value)
+    } catch (error){
+         display.value="Error";
+    }
+}
+
+function clearDisplay() {
+    display.value="";
+}
+
